f449dfd45fb4329c0f1e77f589c5dfa7c6715e63
Raiyan Mahfuz Aulindo <96832621+aulindoraiyan@users.noreply.github.com>
2023-09-12 05:44:24 -0400

    Add files via upload

---
+++ Date/src/Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+    public static void main(String[]args){
+
+        Date aDate = new Date("April", 19, 1960);
+        System.out.println(aDate);
+    }
+    Date aDate = new Date();
+
+}
